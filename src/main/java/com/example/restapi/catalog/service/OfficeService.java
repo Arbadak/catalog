004fbadd963d33cb574ba7e9798d4aa27d8c6ddd
Arbadak <a-timur81@hotmail.com>
2019-01-02 11:23:35 +0500

    Added interfaces for services

--- src/main/java/com/example/restapi/catalog/service/OfficeService.java
+++ src/main/java/com/example/restapi/catalog/service/OfficeService.java
@@ -1,96 +1,20 @@
 package com.example.restapi.catalog.service;
 
-import com.example.restapi.catalog.exceptions.NotFoundException;
-import com.example.restapi.catalog.model.Office;
 import com.example.restapi.catalog.rawModel.RawOffice;
 import com.example.restapi.catalog.rawModel.ResultResponce;
-import com.example.restapi.catalog.repos.OfficeRepo;
-import com.example.restapi.catalog.repos.OrganizationRepo;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class OfficeService {
+/**
+ * Интерфейс с описанием методов для запросов вида /api/office
+ */
+public interface OfficeService {
 
-    private final OrganizationRepo organizationRepo;
-    private final OfficeRepo officeRepo;
+    List<RawOffice> getOfficeList(RawOffice rawOffice);
 
-    @Autowired
-    public OfficeService(OrganizationRepo organizationRepo, OfficeRepo officeRepo) {
-        this.organizationRepo = organizationRepo;
-        this.officeRepo = officeRepo;
-    }
+    RawOffice getOne(Integer officeId);
 
-    public List<RawOffice> getOfficeList(RawOffice rawOffice) {
-        // TODO Фильтрация некорректного orgid
-        List<Office> all = officeRepo.findAllByOrgId(organizationRepo.findOrgByOrgId(rawOffice.getOrgId())); /// создаем список всех оффисов в организации
-        List<RawOffice> result = new ArrayList<>();
+    ResultResponce add(RawOffice office);
 
-        if (rawOffice.getName() != null) {
-            all.removeIf(x -> !rawOffice.getName().equals(x.getName()));
-        }
-        if (rawOffice.getPhone() != null) {
-            all.removeIf(x -> !rawOffice.getName().equals(x.getPhone()));
-        }
-        if (rawOffice.getIsActive() != null) {
-            all.removeIf(x -> !rawOffice.getIsActive().equals(x.getIsActive()));
-        }
-
-        for (Office iterableOffice : all) {        /// перекидываем отфильтованный список в спосок с моделями исключая ненужные поля
-            RawOffice currentRawOffice = new RawOffice();
-            BeanUtils.copyProperties(iterableOffice, currentRawOffice, "orgId", "address", "phone", "isMain");
-            result.add(currentRawOffice);
-        }
-        return result;
-    }
-
-    public RawOffice getOne(Integer officeId) {
-        Office requestedOffice = officeRepo.findById(officeId).orElse(null); ///дурацкий  optional
-        if (requestedOffice == null) { // Если невернвый ид - получи пустой результат
-            return new RawOffice();
-        }
-        RawOffice result = new RawOffice();
-        BeanUtils.copyProperties(requestedOffice, result, "orgId");
-        result.setOrgId(requestedOffice.getOrganizationId().getOrgId());
-        return result;
-    }
-
-    @Transactional
-    public ResultResponce add(RawOffice office) {
-
-        //if (office.getOrgId()==null){return new ResultResponce(null,"не указана организация");}  /// не будем создавать "висячие" офисы
-
-        Office newoffice = new Office();
-        BeanUtils.copyProperties(office, newoffice, "orgId", "Id");
-        newoffice.setOrganizationId(organizationRepo.findOrgByOrgId(office.getOrgId())); ///Выдергиваем организацию из номера в сырой модели, и суем уже обьект
-        officeRepo.save(newoffice);
-        return new ResultResponce("success");
-    }
-
-    @Transactional
-    public ResultResponce update(RawOffice office) {
-
-        /// TODO решить вариант если указали больше параметров чем нужно orgID например
-
-        RawOffice checkoffice = getOne(office.getId());
-        if (checkoffice.getId() == null) {
-            throw new NotFoundException("Офис с указаным id не найден, выйдите в окно");
-        }
-        //if (getOne(office.getId())==null) { throw new NotFoundException("Не сущевствует оффиса с укащанным id");}
-        //return new ResultResponce(null,"не сущевствует оффиса с укащанным id");} /// проверим если ли то что собираемся апдейтить
-        //if (office.getId()==null || office.getName()==null || office.getAddress()==null || office.getIsActive()==null) {return new ResultResponce(null,"не указан обязательный параметр");}
-
-        Office newoffice = officeRepo.findById(office.getId()).orElse(null); //вытскиваем из базы хранимый офис для редактирования
-        BeanUtils.copyProperties(office, newoffice, "orgId", "phone");  //orgid не указывается а phone необязательнй потому их не копируем
-        //newoffice.setOrganizationId(organizationRepo.findOrgByOrgId(office.getOrgId())); ///Выдергиваем организацию из номера в сырой модели, и суем уже обьект
-        newoffice.setPhone((office.getPhone() == (null)) ? getOne(office.getId()).getPhone() : office.getPhone()); /// если не null то заменяем хранящийся телефон
-        officeRepo.save(newoffice);
-        return new ResultResponce("success");
-    }
-
+    ResultResponce update(RawOffice office);
 }
--- src/main/java/com/example/restapi/catalog/service/OrganizationService.java
+++ src/main/java/com/example/restapi/catalog/service/OrganizationService.java
@@ -1,92 +1,21 @@
 package com.example.restapi.catalog.service;
 
-
-import com.example.restapi.catalog.model.Office;
 import com.example.restapi.catalog.model.Organization;
 import com.example.restapi.catalog.rawModel.RawOrganization;
 import com.example.restapi.catalog.rawModel.ResultResponce;
-import com.example.restapi.catalog.repos.OfficeRepo;
-import com.example.restapi.catalog.repos.OrganizationRepo;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Интерфейс с описанием методов для запросов вида /api/organization
+ */
+public interface OrganizationService {
 
-@Service
-public class OrganizationService {
+    List<RawOrganization> getOrgList(RawOrganization rawOrganization);
 
-    private final OrganizationRepo organizationRepo;
-    private final OfficeRepo officeRepo;
+    ResultResponce add(RawOrganization rawOrganization);
 
-    @Autowired
-    public OrganizationService(OrganizationRepo organizationRepo, OfficeRepo officeRepo) {
-        this.organizationRepo = organizationRepo;
-        this.officeRepo = officeRepo;
-    }
+    ResultResponce update(RawOrganization rawOrganization, Organization orgDest);
 
-    public List<RawOrganization> getOrgList(RawOrganization rawOrganization) {
-        List<Organization> all = organizationRepo.findOrgByName(rawOrganization.getName()); //находим все по обязательному полю
-        List<RawOrganization> result = new ArrayList<>();  //лист который вернется пользователю
-        if (rawOrganization.getInn() != null) {  ///Если указан ИНН
-            all.removeIf(x -> !rawOrganization.getInn().equals(x.getInn())); ///Выкидываем всех у кого не такой ИНН как указан
-        }
-
-        for (Iterator<Organization> iterableOrg = all.iterator(); iterableOrg.hasNext(); ) { ///цикл для из списка с учетом фильторв имя и инн, в список который вернется пользователю с учетом поля isActive
-            Organization currentOrg = iterableOrg.next();
-            Office bindedOffice = officeRepo.findByOrgIdAndIsMain(currentOrg, true);
-            if (rawOrganization.getIsActive() != null) { // Если задан isActive
-                if (!bindedOffice.getIsActive().equals(rawOrganization.getIsActive())) {  ///Если статусы несопадают
-                    iterableOrg.remove(); //выкидываем из списка и на следующую итерацию
-                    continue;
-                }
-            }
-            result.add(new RawOrganization(currentOrg.getOrgId(), currentOrg.getName(), bindedOffice.getIsActive()));
-        }
-        return result; ///Возвращаем крокодила пользователю
-    }
-
-    @Transactional
-    public ResultResponce add(RawOrganization rawOrganization) {
-
-        Organization organization = new Organization();
-        BeanUtils.copyProperties(rawOrganization, organization);
-        Organization result = organizationRepo.save(organization);
-        Office mainOffice = new Office(result, rawOrganization.getFullName(), rawOrganization.getAddress(), rawOrganization.getPhone(), rawOrganization.getIsActive(), true);
-        officeRepo.save(mainOffice);
-        return new ResultResponce("success");
-    }
-
-    @Transactional
-    public ResultResponce update(RawOrganization rawOrganization, Organization orgDest) {
-        BeanUtils.copyProperties(rawOrganization, orgDest, "id");
-        Office storedOffice = officeRepo.findByOrgIdAndIsMain(orgDest, true);
-        Office updatingOffice = new Office(storedOffice.getId(), orgDest,  /// Заменяем непередданые данные данными из базы в обновляемом офисе
-                (rawOrganization.getFullName() == (null)) ? rawOrganization.getFullName() : storedOffice.getName(),
-                (rawOrganization.getAddress() == (null)) ? storedOffice.getAddress() : rawOrganization.getAddress(),
-                (rawOrganization.getPhone() == (null)) ? storedOffice.getPhone() : rawOrganization.getPhone(),
-                (rawOrganization.getIsActive() == (null)) ? storedOffice.getIsActive() : rawOrganization.getIsActive(),
-                true);
-        officeRepo.save(updatingOffice);
-        organizationRepo.save(orgDest);
-        return new ResultResponce("success");
-    }
-
-    public RawOrganization getOne(Integer id) {
-        Organization organization = organizationRepo.findOrgByOrgId(id);
-        if (organization == null) {
-            return new RawOrganization();
-        } ///Возвращаем пустую организацию в случае если нет такой
-        Office mainOffice = officeRepo.findByOrgIdAndIsMain(organization, true);
-        RawOrganization result = new RawOrganization();
-        BeanUtils.copyProperties(organization, result);
-        result.setAddress(mainOffice.getAddress());
-        result.setPhone(mainOffice.getPhone());
-        result.setIsActive(mainOffice.getIsActive());
-        return result;
-    }
+    RawOrganization getOne(Integer id);
 }
